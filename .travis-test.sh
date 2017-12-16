a79c7a8ec03bd6e05893f85e2484616a5e8cdba3
Jussi Judin <jjudin+loadavgwatch@iki.fi>
2017-12-16 23:57:26 +0200

    Remove meson build from Travis on OS X

--- .travis-test.sh
+++ .travis-test.sh
@@ -4,11 +4,15 @@ set -xeu
 
 TIMEOUT=$(which timeout 2>/dev/null || which gtimeout 2>/dev/null)
 
-meson build-meson
-ninja -C build-meson
-"$TIMEOUT" 2 build-meson/loadavgwatch --help
-"$TIMEOUT" 2 build-meson/loadavgwatch --version
-"$TIMEOUT" 2 build-meson/loadavgwatch --timeout 0
+# TODO for some reason Meson build on OS X in Travis CI does not
+# recognize fmemopen() function and fails.
+if [[ "$TRAVIS_OS_NAME" != osx ]]; then
+    meson build-meson
+    ninja -C build-meson
+    "$TIMEOUT" 2 build-meson/loadavgwatch --help
+    "$TIMEOUT" 2 build-meson/loadavgwatch --version
+    "$TIMEOUT" 2 build-meson/loadavgwatch --timeout 0
+fi
 
 bazel test --test_output=errors :all
 "$TIMEOUT" 2 bazel-bin/loadavgwatch --help
