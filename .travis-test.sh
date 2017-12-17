4ee5b86aa477e1eac30c3ab92d6325cb851d7d8e
Jussi Judin <jjudin+loadavgwatch@iki.fi>
2017-12-17 03:38:46 +0200

    Add support for pkg-config and more test related Meson build info

--- .travis-test.sh
+++ .travis-test.sh
@@ -4,14 +4,32 @@ set -xeu
 
 TIMEOUT=$(which timeout 2>/dev/null || which gtimeout 2>/dev/null)
 
+WORKDIR=$(mktemp -d)
+cleanup() {
+    rm -rf "$WORKDIR"
+}
+trap cleanup EXIT
+
+test_installation()
+{
+    man loadavgwatch > /dev/null
+    "$TIMEOUT" 2 loadavgwatch --version > /dev/null
+    echo "#include <loadavgwatch.h>" > "$WORKDIR"/header.c
+    echo "void test_lib(void) { loadavgwatch_open(NULL); }" >> "$WORKDIR"/header.c
+    cc -shared -c $(pkg-config --cflags --libs libloadavgwatch) "$WORKDIR"/header.c
+}
+
 # TODO for some reason Meson build on OS X in Travis CI does not
 # recognize fmemopen() function and fails.
-if [[ "$TRAVIS_OS_NAME" != osx ]]; then
+if [ "$TRAVIS_OS_NAME" != osx ]; then
     meson build-meson
     ninja -C build-meson
     "$TIMEOUT" 2 build-meson/loadavgwatch --help
     "$TIMEOUT" 2 build-meson/loadavgwatch --version
     "$TIMEOUT" 2 build-meson/loadavgwatch --timeout 0
+
+    sudo ninja -C build-meson install
+    test_installation
 fi
 
 bazel test --test_output=errors :all
--- README.md
+++ README.md
@@ -5,23 +5,43 @@ exceeds or falls under pre-specified values.
 
 ## Usage
 
-## Building
+## Building and installing
 
 There are several build options that are mainly available for
 situations where loadavgwatch is used as a dependency of a larger
 build sequence.
 
-### Bazel
+### [Bazel](https://bazel.build/)
+
+#### Building and testing
 
 ```bash
 bazel build :all  # Results in binary at bazel-bin/loadavgwatch
+bazel test :all
 ```
 
-### Meson
+### [Meson](http://mesonbuild.com/)
+
+#### Building and testing
 
 ```bash
 meson build-meson
 ninja -C build-meson  # Results in binary at build-meson/loadavgwatch
+ninja -C build-meson test
+```
+
+#### Installing
+
+Installing as a system tool:
+
+```bash
+sudo ninja -C build-meson install
+```
+
+Installing to some specific directory:
+
+```bash
+DESTDIR=/some/path ninja -C build-meson install
 ```
 
 ### Manual builds
